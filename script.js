f32961db678f10f30d37d0f2b220145ad069fb2e
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-30 22:56:13 +0100

    added comments

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const input = document.querySelector("#new-country")
-const chosenCountryList = document.querySelector("#country-info");
-const findCountry = document.querySelector('#find-country');
-const findButton = document.querySelector("#find")
+const findCountry = document.querySelector('#find-button');
+const countryList = document.querySelector("#all-countries")
 
 let countriesInfo;
 let chosenCountryInfo;
@@ -22,37 +21,44 @@ const setUp = ()=>{
 }
 
 const allCountries= (countriesInfo) => {
-    const countryList = document.querySelector("#all-countries")
-    countryList.innerHTML="";
+    
+    countryList.innerHTML=""; // clears, usefully when the method is being used for filtering 
     countriesInfo.forEach((country) => {
 
         const countryItem = document.createElement("li");
         countryItem.innerText = "Name:  "+country.name.common
         countryList.appendChild(countryItem);
-        const countryName = document.createElement("ul");
+        const countryName = document.createElement("ul"); // creates a sub list inside name (countryItem)
         countryItem.appendChild(countryName);
 
         const countryPop = document.createElement("li");
-        countryPop.innerText = "Population:  " + country.population.toLocaleString("en-US");
+        countryPop.innerText = "Population:  " + country.population.toLocaleString("en-US"); // makes population display with commas
         countryName.appendChild(countryPop);
     });
 
 }
 
-findCountry.addEventListener('click', () =>{
+findCountry.addEventListener("click", () =>{ 
+
     const chosenCountry= input.value.toLowerCase();
     filterByCountry(chosenCountry);
-    if(chosenCountryInfo.length>0){
+
+    // if statement for handling an incorrect entry
+    if(chosenCountryInfo.length>0){ 
         allCountries(chosenCountryInfo)
         input.setAttribute("placeholder","Input a country!")
+        input.value="";
     }else{
-        alert("Country not found, try again")
-        input.value=""
+        allCountries(countriesInfo);
+        alert("Country not found, try again")// gives a pop up to users
+        input.value="";
         input.setAttribute("placeholder","not found try again")
     }
 })
 
 const filterByCountry = (chosenCountry) =>{
+    // includes() also gives back any countries where the characters are also in the name i.e if "us" is sent 
+    // then austria and cyprus is added etc.
     chosenCountryInfo= countriesInfo.filter((country)=> 
         country.name.common.toLowerCase().includes(chosenCountry));
 }
