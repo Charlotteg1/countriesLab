cacf4dabe75ec6378551350eef391a4dd9eea72e
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-30 22:14:19 +0100

    seperated into different methods and added filter

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
+const input = document.querySelector("#new-country")
+const chosenCountryList = document.querySelector("#country-info");
+const findCountry = document.querySelector('#find-country');
+const findButton = document.querySelector("#find")
 
+let countriesInfo;
+let chosenCountryInfo;
+let found;
 
+const fetchCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+} 
 
-
-
-const setUp = async ()=>{
+const setUp = ()=>{
     fetchCountries()
+    .then((jsonData) => {
+        countriesInfo=jsonData;
+        allCountries(countriesInfo)
+    })
+    enterButton();
 }
 
-const fetchCountries= async () => {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    let jsonData = await response.json();
-    console.log(jsonData);
-
-    const countryList = document.querySelector("ul")
-
-    jsonData.forEach((country) => {
+const allCountries= (countriesInfo) => {
+    const countryList = document.querySelector("#all-countries")
+    countryList.innerHTML="";
+    countriesInfo.forEach((country) => {
         const countryItem = document.createElement("li");
-        countryItem.textContent = "Name:  "+country.name.common 
-        // +"\n"+ " Population:" + country.population;
+        countryItem.innerText = "Name:  "+country.name.common
         countryList.appendChild(countryItem);
         const countryName = document.createElement("ul");
         const countryPop = document.createElement("li");
-        countryPop.textContent = "Population:  " + country.population.toLocaleString("en-US");
+        countryPop.innerText = "Population:  " + country.population.toLocaleString("en-US");
         countryItem.appendChild(countryName);
         countryName.appendChild(countryPop);
     });
-    
 
+}
+
+const enterButton=()=> {
+    findCountry.addEventListener('click', () =>{
+        const chosenCountry= input.value.toLowerCase();
+        filterByCountry(chosenCountry);
+        if(chosenCountryInfo.length>0){
+            
+        allCountries(chosenCountryInfo)
+        input.setAttribute("placeholder","Input a country!")
+        }else{
+            input.value=""
+            input.setAttribute("placeholder","not found try again")
+        }
+
+    })
+}
+
+const filterByCountry = (chosenCountry) =>{
+    chosenCountryInfo= countriesInfo.filter((country)=> country.name.common.toLowerCase().includes(chosenCountry));
+    
 }
 
 
 setUp()
+
 
